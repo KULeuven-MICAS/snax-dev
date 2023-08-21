ac6776894850e2e6beb3120e14f6e05c3b527568
rgantonio <ryan.antonio@esat.kuleuven.be>
2023-08-21 16:18:48 +0200

    tests: Add synthetic memory binaries

---
+++ tests/mem/asm/addi_only.s
@@ -0,0 +1,25 @@
+# ---------------------------------------------
+# Copyright 2023 Katolieke Universiteit Leuven (KUL)
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+# Author: Ryan Antonio (ryan.antonio@kuleuven.be)
+# ---------------------------------------------
+nop
+nop
+nop
+nop
+nop
+addi x1, x0, 100
+addi x2, x0, 100
+add  x3, x1, x2
+addi x4, x0, 200
+addi x5, x0, 300
+add  x6, x4, x5
+addi x7, x0, 255
+addi x8, x0, 1024
+add  x9, x7, x8
+nop
+nop
+nop
+nop
+nop
