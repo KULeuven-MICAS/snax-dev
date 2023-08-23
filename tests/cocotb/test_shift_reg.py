81e215f59f70f857e51630b2f611df2c9679484b
rgantonio <ryan.antonio@esat.kuleuven.be>
2023-08-23 15:30:50 +0200

    tests: Fix test_shift_reg.py comments

--- tests/cocotb/test_shift_reg.py
+++ tests/cocotb/test_shift_reg.py
@@ -5,9 +5,6 @@
 # Author: Ryan Antonio (ryan.antonio@esat.kuleuven.be)
 # ---------------------------------
 
-# -----------------------------------
-# Imports
-# -----------------------------------
 import subprocess
 import cocotb
 from cocotb.triggers import RisingEdge
@@ -25,44 +22,37 @@ DATA_WIDTH = 16
 
 
 # -----------------------------------
-# Stimulus
+# Test stimulus
 # -----------------------------------
 @cocotb.test()
 async def shift_reg_dut(dut):
-    # Initialize clock
     clock = Clock(dut.clk_i, 10, units="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset or intial values
     dut.rst_ni.value = 0
 
-    # Wait 1 cycle to reset
     await RisingEdge(dut.clk_i)
 
-    # Deassert reset
     dut.rst_ni.value = 1
 
-    # Getting check list
     checker = [0] * (FIFO_DEPTH + 1)
     answer = []
 
-    # Iterate test cases
     for i in range(TEST_COUNT):
         input_val = random.randint(0, (2**DATA_WIDTH - 1))
         dut.d_i.value = input_val
 
         output_val = dut.d_o.value
 
-        # Workaround so that both Verilator and Modelsim pass
+        # Workaround since Verilator only outputs 1s and 0s
+        # While modelsim outputs don't care X's
         if str(output_val).isnumeric():
             output_val = int(output_val)
         else:
             output_val = 0
 
-        # Log debug
         cocotb.log.info(f"Shift reg input: {input_val}; Shift reg output: {output_val}")
 
-        # Collect answers and expected outputs
         checker.append(input_val)
         answer.append(output_val)
 
@@ -72,19 +62,14 @@ async def shift_reg_dut(dut):
 
 
 # -----------------------------------
-# Parameter inputs to DUT
+# Test build
 # -----------------------------------
 @pytest.mark.parametrize(
     "parameters", [{"DataWidth": str(DATA_WIDTH), "Depth": str(FIFO_DEPTH)}]
 )
-
-# -----------------------------------
-# Run setup
-# -----------------------------------
 def test_shift_reg(parameters, simulator):
     # RTL paths
     # Extract paths for benderized files
-    # This way is also seen on github actions
     common_cells_dir = subprocess.run(
         ["bender", "path", "common_cells"], stdout=subprocess.PIPE
     )
@@ -97,17 +82,14 @@ def test_shift_reg(parameters, simulator):
         "tests/tb/tb_shift_reg.sv",
     ]
 
-    # Include directories
     include_folders = [common_cells_dir + "/include"]
 
-    # Specify top-level module
     toplevel = "tb_shift_reg"
 
-    # Specify python test name that contains the @cocotb.test.
-    # Usually the name of this test.
+    # Module is the python test name that contains the @cocotb.test.
+    # In this example, it's the name of this test
     module = "test_shift_reg"
 
-    # Specify build directory
     sim_build = "tests/sim_build/{}/".format(toplevel)
 
     run(
