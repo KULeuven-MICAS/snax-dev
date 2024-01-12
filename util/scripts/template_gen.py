4ed015c0a319ae10afa9f30381cab72cc2fe3637
rgantonio <ryan.antonio@esat.kuleuven.be>
2024-01-12 10:38:10 +0100

    util: Create template generator

---
+++ util/scripts/template_gen.py
@@ -0,0 +1,47 @@
+from mako.lookup import TemplateLookup
+from mako.template import Template
+from jsonref import JsonRef
+from typing import Union, List, Dict
+import hjson
+import argparse
+
+# Extract json file
+def get_config (cfg_path: str):
+    with open(cfg_path, "r") as jsonf:
+        srcfull = jsonf.read()
+
+    # Format hjson file
+    cfg = hjson.loads(srcfull, use_decimal=True)
+    cfg = JsonRef.replace_refs(cfg)
+    return cfg
+
+# Read template
+def get_template (tpl_path: str) -> Template:
+    tpl_list = TemplateLookup(directories=["."], output_encoding="utf-8")
+    tpl = tpl_list.get_template(tpl_path)
+    return tpl
+
+# Generate file
+def gen_file (cfg, tpl, target_path: str) -> None:
+    with open(target_path, "w") as f:
+        f.write(str(tpl.render_unicode(cfg=cfg)))
+    return
+
+# Main function run and parsing
+def main():
+    # Parse all arguments
+    parser = argparse.ArgumentParser(description='Wrapper generator for any file. Inputs are simply the template and configuration files.')
+    parser.add_argument('--cfg_path', type=str, default='./', help='Points to the configuration file path')
+    parser.add_argument('--tpl_path', type=str, default='./', help='Points to the template file path')
+    parser.add_argument('--out_path', type=str, default='./', help='Points to the output directory')
+
+    # Get the list of parsing
+    args = parser.parse_args()
+
+    # Grab config and template then generate the combination of two
+    cfg = get_config(args.cfg_path)
+    tpl = get_template(args.tpl_path)
+    gen_file(cfg=cfg, tpl=tpl, target_path=args.out_path)
+
+if __name__ == "__main__":
+    main()
