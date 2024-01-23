382060440ef6fdd361881f3cb1022436409e237f
rgantonio <ryan.antonio@esat.kuleuven.be>
2024-01-23 10:45:29 +0100

    py: Add waves option

--- conftest.py
+++ conftest.py
@@ -8,8 +8,19 @@ def pytest_addoption(parser):
         default="verilator",
         help="simulator used by cocotb",
     )
+    parser.addoption(
+        "--waves",
+        dest="waves",
+        default=0,
+        help="enabling wave generation. verilator generates .fst; modelsim generats .wlf ",
+    )
 
 
 @pytest.fixture
 def simulator(request):
     return request.config.getoption("--simulator")
+
+
+@pytest.fixture
+def waves(request):
+    return request.config.getoption("--waves")
