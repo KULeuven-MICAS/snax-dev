5194cc0e574f476293f2757df0629044cdc42f5b
Ryan Antonio <ryan.antonio@esat.kuleuven.be>
2024-01-24 14:40:13 +0100

    Add DMA to TCDM subsystem test (#21)
    * tests: Add tcdm functions
    
    * rtl: Update tcdm subsystem to have DMA port
    
    * test: Add DMA check to TCDM subsys
    
    * tests: Update conections to tb TCDM subsys
    
    * ci: Take out logging in CI
    
    * rtl: Code clean up for SV file
    
    * test: Added comments
    
    * test: Added comments

--- rtl/memory-subsys/tcdm_subsys.sv
+++ rtl/memory-subsys/tcdm_subsys.sv
@@ -17,22 +17,27 @@
 // TCDMSize - total size of TCDM memory
 // TCDMAddrWidth - total address width of memory
 // NumInp - number of requesters (core or accelerator)
-// NumOut - number of ports connected to memory
-//        - this one should actually be equal to NrBanks
 //-----------------------------------
 
 module tcdm_subsys #(
   parameter int unsigned NarrowDataWidth  = 64,
   parameter int unsigned TCDMDepth        = 512,
   parameter int unsigned NrBanks          = 32,
+  parameter int unsigned WideDataWidth    = 512, // Need to set wide data width to max bits of banks
   parameter int unsigned TCDMMemAddrWidth = $clog2(TCDMDepth),
   parameter int unsigned TCDMSize         = NrBanks * TCDMDepth * (NarrowDataWidth/8),
   parameter int unsigned TCDMAddrWidth    = $clog2(TCDMSize),
-  parameter int unsigned NumInp           = 2,
-  parameter int unsigned NumOut           = 2
+  parameter int unsigned NumInp           = 2
 )(
-  input  logic                                      clk_i,
-  input  logic                                      rst_ni,
+  //-----------------------------
+  // Clocks and Reset
+  //-----------------------------
+  input  logic  clk_i,
+  input  logic  rst_ni,
+
+  //-----------------------------
+  // TCDM ports
+  //-----------------------------
   input  logic  [NumInp-1:0]                        tcdm_req_write_i,
   input  logic  [NumInp-1:0][TCDMAddrWidth-1:0]     tcdm_req_addr_i,
   //Note that tcdm_req_amo_i is 4 bits based on reqrsp definition
@@ -45,11 +50,28 @@ module tcdm_subsys #(
   input  logic  [NumInp-1:0]                        tcdm_req_q_valid_i,
   output logic  [NumInp-1:0]                        tcdm_rsp_q_ready_o,
   output logic  [NumInp-1:0]                        tcdm_rsp_p_valid_o,
-  output logic  [NumInp-1:0][NarrowDataWidth-1:0]   tcdm_rsp_data_o
+  output logic  [NumInp-1:0][NarrowDataWidth-1:0]   tcdm_rsp_data_o,
+
+  //-----------------------------
+  // Wide TCDM ports
+  //-----------------------------
+  input  logic                        tcdm_dma_req_write_i,
+  input  logic  [  TCDMAddrWidth-1:0] tcdm_dma_req_addr_i,
+  input  logic  [  WideDataWidth-1:0] tcdm_dma_req_data_i,
+  input  logic  [WideDataWidth/8-1:0] tcdm_dma_req_strb_i,
+  input  logic                        tcdm_dma_req_q_valid_i,
+  output logic                        tcdm_dma_rsp_q_ready_o,
+  output logic                        tcdm_dma_rsp_p_valid_o,
+  output logic  [  WideDataWidth-1:0] tcdm_dma_rsp_data_o
 );
 
+  localparam int unsigned BanksPerSuperBank = WideDataWidth/NarrowDataWidth;
+  localparam int unsigned NrSuperBanks      = NrBanks/BanksPerSuperBank;
+
   typedef logic [  NarrowDataWidth-1:0] data_t;
+  typedef logic [    WideDataWidth-1:0] data_dma_t;
   typedef logic [NarrowDataWidth/8-1:0] strb_t;
+  typedef logic [  WideDataWidth/8-1:0] strb_dma_t;
   typedef logic [    TCDMAddrWidth-1:0] tcdm_addr_t;
   typedef logic [ TCDMMemAddrWidth-1:0] tcdm_mem_addr_t;
 
@@ -73,12 +95,22 @@ module tcdm_subsys #(
   `TCDM_TYPEDEF_ALL (tcdm, tcdm_addr_t, data_t, strb_t, tcdm_user_t)
   `MEM_TYPEDEF_ALL (mem, tcdm_mem_addr_t, data_t, strb_t, tcdm_user_t)
 
-  // Main connections
+  `TCDM_TYPEDEF_ALL(tcdm_dma, tcdm_addr_t, data_dma_t, strb_dma_t, logic)
+  `MEM_TYPEDEF_ALL (mem_dma, tcdm_mem_addr_t, data_dma_t, strb_dma_t, logic)
+
+  // Main connections for narrow TCDM
   tcdm_req_t [NumInp-1:0] tcdm_req;
   tcdm_rsp_t [NumInp-1:0] tcdm_rsp;
 
-  mem_req_t  [NumOut-1:0] mem_req;
-  mem_rsp_t  [NumOut-1:0] mem_rsp;
+  mem_req_t [NrSuperBanks-1:0][BanksPerSuperBank-1:0] narrow_req;
+  mem_rsp_t [NrSuperBanks-1:0][BanksPerSuperBank-1:0] narrow_rsp;
+
+  // Main connections for wide TCDM
+  tcdm_dma_req_t tcdm_dma_req;
+  tcdm_dma_rsp_t tcdm_dma_rsp;
+
+  mem_dma_req_t [NrSuperBanks-1:0] wide_dma_req;
+  mem_dma_rsp_t [NrSuperBanks-1:0] wide_dma_rsp;
 
   // Hard re-mapping of TCDM req and rsp
   // To make the control ports more generic
@@ -121,9 +153,29 @@ module tcdm_subsys #(
     end
   end
 
+  always_comb begin
+
+    // These signals are never used
+    tcdm_dma_req.q.amo     = reqrsp_pkg::AMONone;
+    tcdm_dma_req.q.user    = '0;
+
+    // Remapping for visibility
+    // Request (incoming) remapping
+    tcdm_dma_req.q.write   = tcdm_dma_req_write_i;
+    tcdm_dma_req.q.addr    = tcdm_dma_req_addr_i;
+    tcdm_dma_req.q.data    = tcdm_dma_req_data_i;
+    tcdm_dma_req.q.strb    = tcdm_dma_req_strb_i;
+    tcdm_dma_req.q_valid   = tcdm_dma_req_q_valid_i;
+
+    // Response (outgoing) remapping
+    tcdm_dma_rsp_q_ready_o = tcdm_dma_rsp.q_ready;
+    tcdm_dma_rsp_p_valid_o = tcdm_dma_rsp.p_valid;
+    tcdm_dma_rsp_data_o    = tcdm_dma_rsp.p.data;
+  end
+
   snitch_tcdm_interconnect #(
     .NumInp                ( NumInp                              ),
-    .NumOut                ( NumOut                              ),
+    .NumOut                ( NrBanks                             ),
     .tcdm_req_t            ( tcdm_req_t                          ),
     .tcdm_rsp_t            ( tcdm_rsp_t                          ),
     .mem_req_t             ( mem_req_t                           ),
@@ -139,81 +191,129 @@ module tcdm_subsys #(
     .rst_ni                ( rst_ni                              ),
     .req_i                 ( tcdm_req                            ),
     .rsp_o                 ( tcdm_rsp                            ),
-    .mem_req_o             ( mem_req                             ),
-    .mem_rsp_i             ( mem_rsp                             )
+    .mem_req_o             ( narrow_req                          ),
+    .mem_rsp_i             ( narrow_rsp                          )
   );
 
-  // Generate multi-bank memories
-  // Number of banks matches number of memories
-  for (genvar i = 0; i < NumOut; i++) begin : gen_tcdm_bank
+  snitch_tcdm_interconnect #(
+    .NumInp                ( 1                                   ),
+    .NumOut                ( NrSuperBanks                        ),
+    .tcdm_req_t            ( tcdm_dma_req_t                      ),
+    .tcdm_rsp_t            ( tcdm_dma_rsp_t                      ),
+    .mem_req_t             ( mem_dma_req_t                       ),
+    .mem_rsp_t             ( mem_dma_rsp_t                       ),
+    .MemAddrWidth          ( TCDMMemAddrWidth                    ),
+    .DataWidth             ( WideDataWidth                       ),
+    .user_t                ( logic                               ),
+    .MemoryResponseLatency ( 1                                   ),
+    .Radix                 ( 2                                   ),
+    .Topology              ( snitch_pkg::LogarithmicInterconnect )
+  ) i_dma_tcdm_interconnect (
+    .clk_i                 ( clk_i                               ),
+    .rst_ni                ( rst_ni                              ),
+    .req_i                 ( tcdm_dma_req                        ),
+    .rsp_o                 ( tcdm_dma_rsp                        ),
+    .mem_req_o             ( wide_dma_req                        ),
+    .mem_rsp_i             ( wide_dma_rsp                        )
+  );
 
-    logic           mem_cs;
-    logic           mem_wen;
-    tcdm_mem_addr_t mem_add;
-    strb_t          mem_be;
-    data_t          mem_rdata;
-    data_t          mem_wdata;
-    data_t          amo_rdata_local;
 
-    tc_sram_impl #(
-      .NumWords   ( TCDMDepth       ),
-      .DataWidth  ( NarrowDataWidth ),
-      .ByteWidth  ( 8               ),
-      .NumPorts   ( 1               ),
-      .SimInit    ( "zeros"         ),
-      .Latency    ( 1               ),
-      .impl_in_t  ( sram_cfg_t      )
-    ) i_data_mem (
-      .clk_i      ( clk_i           ),
-      .rst_ni     ( rst_ni          ),
-      .impl_i     ( sram_cfgs.tcdm  ),
-      .impl_o     (                 ), //Unused since it's SRAM dependent
-      .req_i      ( mem_cs          ),
-      .we_i       ( mem_wen         ),
-      .addr_i     ( mem_add         ),
-      .wdata_i    ( mem_wdata       ),
-      .be_i       ( mem_be          ),
-      .rdata_o    ( mem_rdata       )
+  for(genvar i = 0; i < NrSuperBanks; i++) begin: gen_tcdm_super_bank
+
+    mem_req_t [BanksPerSuperBank-1:0] amo_req;
+    mem_rsp_t [BanksPerSuperBank-1:0] amo_rsp;
+
+    mem_wide_narrow_mux #(
+      .NarrowDataWidth    ( NarrowDataWidth         ),
+      .WideDataWidth      ( WideDataWidth           ),
+      .mem_narrow_req_t   ( mem_req_t               ),
+      .mem_narrow_rsp_t   ( mem_rsp_t               ),
+      .mem_wide_req_t     ( mem_dma_req_t           ),
+      .mem_wide_rsp_t     ( mem_dma_rsp_t           )
+    ) i_tcdm_mux (
+      .clk_i              ( clk_i                   ),
+      .rst_ni             ( rst_ni                  ),
+      .in_narrow_req_i    ( narrow_req[i]           ),
+      .in_narrow_rsp_o    ( narrow_rsp[i]           ),
+      .in_wide_req_i      ( wide_dma_req[i]         ),
+      .in_wide_rsp_o      ( wide_dma_rsp[i]         ),
+      .out_req_o          ( amo_req                 ),
+      .out_rsp_i          ( amo_rsp                 ),
+      .sel_wide_i         ( wide_dma_req[i].q_valid )
     );
 
-    // Atomic memory operations
-    snitch_amo_shim #(
-      .AddrMemWidth   ( TCDMMemAddrWidth          ),
-      .DataWidth      ( NarrowDataWidth           ),
-      .CoreIDWidth    ( 5                         )
-    ) i_amo_shim (
-      .clk_i          ( clk_i                     ),
-      .rst_ni         ( rst_ni                    ),
-      .valid_i        ( mem_req[i].q_valid        ),
-      .ready_o        ( mem_rsp[i].q_ready        ),
-      .addr_i         ( mem_req[i].q.addr         ),
-      .write_i        ( mem_req[i].q.write        ),
-      .wdata_i        ( mem_req[i].q.data         ),
-      .wstrb_i        ( mem_req[i].q.strb         ),
-      .core_id_i      ( mem_req[i].q.user.core_id ),
-      .is_core_i      ( mem_req[i].q.user.is_core ),
-      .rdata_o        ( amo_rdata_local           ),
-      .amo_i          ( mem_req[i].q.amo          ),
-      .mem_req_o      ( mem_cs                    ),
-      .mem_add_o      ( mem_add                   ),
-      .mem_wen_o      ( mem_wen                   ),
-      .mem_wdata_o    ( mem_wdata                 ),
-      .mem_be_o       ( mem_be                    ),
-      .mem_rdata_i    ( mem_rdata                 ),
-      .dma_access_i   ( 1'b0                      ), // Unused since we don't simulate DMA here
-      .amo_conflict_o (                           )
-    );
+    // Generate multi-bank memories
+    // Number of banks matches number of memories
+    for (genvar j = 0; j < BanksPerSuperBank; j++) begin : gen_tcdm_bank
 
-    // Insert a pipeline register at the output of each SRAM.
-    shift_reg #(
-      .dtype  ( data_t            ),
-      .Depth  ( 0                 ) // Unused for now
-    ) i_sram_pipe (
-      .clk_i  ( clk_i             ),
-      .rst_ni ( rst_ni            ),
-      .d_i    ( amo_rdata_local   ),
-      .d_o    ( mem_rsp[i].p.data )
-    );
+      logic           mem_cs;
+      logic           mem_wen;
+      tcdm_mem_addr_t mem_add;
+      strb_t          mem_be;
+      data_t          mem_rdata;
+      data_t          mem_wdata;
+      data_t          amo_rdata_local;
+
+      tc_sram_impl #(
+        .NumWords   ( TCDMDepth       ),
+        .DataWidth  ( NarrowDataWidth ),
+        .ByteWidth  ( 8               ),
+        .NumPorts   ( 1               ),
+        .SimInit    ( "zeros"         ),
+        .Latency    ( 1               ),
+        .impl_in_t  ( sram_cfg_t      )
+      ) i_data_mem (
+        .clk_i      ( clk_i           ),
+        .rst_ni     ( rst_ni          ),
+        .impl_i     ( sram_cfgs.tcdm  ),
+        .impl_o     (                 ), //Unused since it's SRAM dependent
+        .req_i      ( mem_cs          ),
+        .we_i       ( mem_wen         ),
+        .addr_i     ( mem_add         ),
+        .wdata_i    ( mem_wdata       ),
+        .be_i       ( mem_be          ),
+        .rdata_o    ( mem_rdata       )
+      );
+
+      // Atomic memory operations
+      snitch_amo_shim #(
+        .AddrMemWidth   ( TCDMMemAddrWidth          ),
+        .DataWidth      ( NarrowDataWidth           ),
+        .CoreIDWidth    ( 5                         )
+      ) i_amo_shim (
+        .clk_i          ( clk_i                     ),
+        .rst_ni         ( rst_ni                    ),
+        .valid_i        ( amo_req[j].q_valid        ),
+        .ready_o        ( amo_rsp[j].q_ready        ),
+        .addr_i         ( amo_req[j].q.addr         ),
+        .write_i        ( amo_req[j].q.write        ),
+        .wdata_i        ( amo_req[j].q.data         ),
+        .wstrb_i        ( amo_req[j].q.strb         ),
+        .core_id_i      ( amo_req[j].q.user.core_id ),
+        .is_core_i      ( amo_req[j].q.user.is_core ),
+        .rdata_o        ( amo_rdata_local           ),
+        .amo_i          ( amo_req[j].q.amo          ),
+        .mem_req_o      ( mem_cs                    ),
+        .mem_add_o      ( mem_add                   ),
+        .mem_wen_o      ( mem_wen                   ),
+        .mem_wdata_o    ( mem_wdata                 ),
+        .mem_be_o       ( mem_be                    ),
+        .mem_rdata_i    ( mem_rdata                 ),
+        .dma_access_i   ( wide_dma_req[i].q_valid   ), // Unused since we don't simulate DMA here
+        .amo_conflict_o (                           )
+      );
+
+      // Insert a pipeline register at the output of each SRAM.
+      shift_reg #(
+        .dtype  ( data_t            ),
+        .Depth  ( 0                 ) // Unused for now
+      ) i_sram_pipe (
+        .clk_i  ( clk_i             ),
+        .rst_ni ( rst_ni            ),
+        .d_i    ( amo_rdata_local   ),
+        .d_o    ( amo_rsp[j].p.data )
+      );
+    end
   end
 
 endmodule
--- tests/cocotb/test_tcdm_subsys.py
+++ tests/cocotb/test_tcdm_subsys.py
@@ -10,29 +10,50 @@
 # ---------------------------------
 
 import cocotb
-from cocotb.triggers import RisingEdge, Timer
 from cocotb.clock import Clock
 from cocotb_test.simulator import run
-from decimal import Decimal
 import pytest
 import snax_util
+import math
 
 # Configurable design time parameters
 NARROW_DATA_WIDTH = 64
+WIDE_DATA_WIDTH = 512
 TCDM_DEPTH = 64
-NR_BANKS = 8
+NR_BANKS = 32
 NUM_INPUT = 2
+
+# Configuration checker for the DMA wide
+# TCDM interconnection because
+# We need to make sure that the memory
+# and interconnect matches the addressing
+# note that this is an internal checker
+TCDM_SIZE = int(NR_BANKS * TCDM_DEPTH * NARROW_DATA_WIDTH / 8)
+TCDM_ADDR_WIDTH = math.ceil(math.log2(TCDM_SIZE))
+TCDM_MEM_ADDR_WIDTH = math.ceil(math.log2(TCDM_DEPTH))
+NR_BANKS_PER_SUPERBANK = WIDE_DATA_WIDTH / NARROW_DATA_WIDTH
+NR_SUPER_BANKS = int(NR_BANKS / NR_BANKS_PER_SUPERBANK)
+WIDE_TCDM_PORTS = math.ceil(math.log2(NR_SUPER_BANKS))
+WIDE_BYTE_OFFSET = math.ceil(math.log2(int(WIDE_DATA_WIDTH / 8)))
+
+assert TCDM_ADDR_WIDTH >= (
+    TCDM_MEM_ADDR_WIDTH + WIDE_TCDM_PORTS + WIDE_BYTE_OFFSET
+), "Error config! Make sure to satisfy the assertion equation"
 
 # Optinally configurable parameters
 NUM_OUTPUT = NR_BANKS
 MIN_VAL = 0
-MAX_VAL = 2**NARROW_DATA_WIDTH
-BANK_INCREMENT = NARROW_DATA_WIDTH / 8
+MAX_VAL = 2**NARROW_DATA_WIDTH - 1
+WIDE_MIN_VAL = 0
+WIDE_MAX_VAL = 2**WIDE_DATA_WIDTH - 1
+BANK_INCREMENT = int(NARROW_DATA_WIDTH / 8)
+WIDE_BANK_INCREMENT = int(BANK_INCREMENT * (WIDE_DATA_WIDTH / NARROW_DATA_WIDTH))
 
 # Configurable testing parameters
 # In the default value below, the number
 # of tests fills the entire memory
-NUM_TESTS = TCDM_DEPTH * NR_BANKS
+NUM_NARROW_TESTS = TCDM_DEPTH * NR_BANKS
+NUM_WIDE_TESTS = int(NUM_NARROW_TESTS / 8)
 
 
 @cocotb.test()
@@ -60,65 +81,73 @@ async def tcdm_subsys_dut(dut):
         dut.tcdm_req_strb_i[i].value = 0
         dut.tcdm_req_q_valid_i[i].value = 0
 
-    await RisingEdge(dut.clk_i)
-    await Timer(Decimal(1), units="ns")
+    dut.tcdm_dma_req_write_i.value = 0
+    dut.tcdm_dma_req_addr_i.value = 0
+    dut.tcdm_dma_req_data_i.value = 0
+    dut.tcdm_dma_req_strb_i.value = 0
+    dut.tcdm_dma_req_q_valid_i.value = 0
+
+    await snax_util.clock_and_wait(dut)
 
     # Release reset
     dut.rst_ni.value = 1
 
-    await RisingEdge(dut.clk_i)
-    await Timer(Decimal(1), units="ns")
+    await snax_util.clock_and_wait(dut)
 
     # Begin test
+    cocotb.log.info(" ------------------------------------------ ")
+    cocotb.log.info(" Testing for TCDM request and response ports")
+    cocotb.log.info(" ------------------------------------------ ")
     for i in range(NUM_INPUT):
-        golden_list = snax_util.gen_rand_int_list(NUM_TESTS, MIN_VAL, MAX_VAL)
+        golden_list = snax_util.gen_rand_int_list(NUM_NARROW_TESTS, MIN_VAL, MAX_VAL)
 
         # Cycle through values that change
         # Per item or element
         # Explicitly write the control signals
-        for j in range(len(golden_list)):
-            dut.tcdm_req_addr_i[i].value = int(j * BANK_INCREMENT)
-            dut.tcdm_req_data_i[i].value = golden_list[j]
-            dut.tcdm_req_strb_i[i].value = 0xFF
-            dut.tcdm_req_write_i[i].value = 1
-            dut.tcdm_req_q_valid_i[i].value = 1
-            await RisingEdge(dut.clk_i)
-            await Timer(Decimal(1), units="ns")
+        for j in range(NUM_NARROW_TESTS):
+            await snax_util.tcdm_write(dut, i, int(j * BANK_INCREMENT), golden_list[j])
 
         # Clear default inputs for reading
-        dut.tcdm_req_addr_i[i].value = 0
-        dut.tcdm_req_data_i[i].value = 0
-        dut.tcdm_req_strb_i[i].value = 0
-        dut.tcdm_req_write_i[i].value = 0
-        dut.tcdm_req_q_valid_i[i].value = 0
-        await RisingEdge(dut.clk_i)
-        await Timer(Decimal(1), units="ns")
+        await snax_util.tcdm_clr(dut, i)
 
         # Cycle through reads
         # And check immediately if result is correct
-        for j in range(len(golden_list)):
-            dut.tcdm_req_addr_i[i].value = int(j * BANK_INCREMENT)
-            dut.tcdm_req_data_i[i].value = 0
-            dut.tcdm_req_strb_i[i].value = 0
-            dut.tcdm_req_write_i[i].value = 0
-            dut.tcdm_req_q_valid_i[i].value = 1
-            await RisingEdge(dut.clk_i)
-            await Timer(Decimal(1), units="ns")
-
+        for j in range(NUM_NARROW_TESTS):
+            check_val = await snax_util.tcdm_read(dut, i, int(j * BANK_INCREMENT))
             # Check for results
-            check_val = int(dut.tcdm_rsp_data_o[i].value)
             cocotb.log.info(f"Port {i} Actual output: {check_val}")
             cocotb.log.info(f"Golden output: {golden_list[j]}")
             assert check_val == golden_list[j]
 
         # Clear default inputs for reading
-        dut.tcdm_req_addr_i[i].value = 0
-        dut.tcdm_req_data_i[i].value = 0
-        dut.tcdm_req_strb_i[i].value = 0
-        dut.tcdm_req_write_i[i].value = 0
-        dut.tcdm_req_q_valid_i[i].value = 0
-        await RisingEdge(dut.clk_i)
-        await Timer(Decimal(1), units="ns")
+        await snax_util.tcdm_clr(dut, i)
+
+    cocotb.log.info(" ------------------------------------------ ")
+    cocotb.log.info(" Wide TCDM tests for the DMA transfers")
+    cocotb.log.info(" ------------------------------------------ ")
+
+    # Get golden data
+    wide_golden_list = snax_util.gen_rand_int_list(
+        NUM_WIDE_TESTS, WIDE_MIN_VAL, WIDE_MAX_VAL
+    )
+
+    # Write data to TCDM
+    for i in range(NUM_WIDE_TESTS):
+        await snax_util.wide_tcdm_write(
+            dut, i * WIDE_BANK_INCREMENT, wide_golden_list[i]
+        )
+        print(i * WIDE_BANK_INCREMENT)
+
+    # Write to clear
+    await snax_util.wide_tcdm_clr(dut)
+
+    # Read data from TCDM
+    for i in range(NUM_WIDE_TESTS):
+        check_val = await snax_util.wide_tcdm_read(dut, int(i * WIDE_BANK_INCREMENT))
+        # Check for results
+        cocotb.log.info(f"Actual output: {check_val}")
+        cocotb.log.info(f"Golden output: {wide_golden_list[i]}")
+        assert check_val == wide_golden_list[i]
 
 
 # Main test run
@@ -127,10 +156,10 @@ async def tcdm_subsys_dut(dut):
     [
         {
             "NarrowDataWidth": str(NARROW_DATA_WIDTH),
+            "WideDataWidth": str(WIDE_DATA_WIDTH),
             "TCDMDepth": str(TCDM_DEPTH),
             "NrBanks": str(NR_BANKS),
             "NumInp": str(NUM_INPUT),
-            "NumOut": str(NUM_OUTPUT),
         }
     ],
 )
@@ -178,4 +207,5 @@ def test_tcdm_subsys(parameters, simulator):
         sim_build=sim_build,
         compile_args=compile_args,
         timescale=timescale,
+        parameters=parameters,
     )
